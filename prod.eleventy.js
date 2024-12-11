176c429947424bfe1807b55f70a5c40174766000
Stefenmarg <stefenmarg@protonmail.com>
2024-12-11 23:49:00 +0200

    Added new RSS feed generator & content

--- prod.eleventy.js
+++ prod.eleventy.js
@@ -1,17 +1,23 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const xmlPlugin = require("eleventy-xml-plugin");
 const registerCollections = require('./collections');
 
 module.exports = function (eleventyConfig) {
     //All the plugins
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(xmlPlugin);
+    
+    //Additional tempate formats used to build this site
+    eleventyConfig.addTemplateFormats("xml");
+
+    //Filters that are used in the site
+    eleventyConfig.addFilter("dateToRfc2822", function(dateObj) {
+        return new Date(dateObj).toUTCString();
+    });
     
     //Files to copy to the destination without processing
 	eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/js/snowstorm.js');
-
-    //Watch for any file change 
-    eleventyConfig.addWatchTarget('src/js');
-	eleventyConfig.addWatchTarget('src/collections');
     
     //Collection management
     registerCollections(eleventyConfig, 15);
