55790eaff540f99950b7ffcaccbda22888cac39a
Stefanos M. <stefenmarg@protonmail.com>
2025-01-30 23:35:42 +0200

    Changed output directory again, hopefully fixing the site's issues.

--- eleventy.config.js
+++ eleventy.config.js
@@ -34,7 +34,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "Source",
-            output: "_docs",
+            output: "docs",
         },
     }
 };
